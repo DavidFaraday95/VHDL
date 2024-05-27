c39947d39828e008f7d7344f6552c31f8cf70a99
DavidFaraday95 <46838417+DavidFaraday95@users.noreply.github.com>
2024-05-27 21:14:45 +0200

    Create 05_Synchronous_Asynchronous.vhd

---
+++ Basics/05_Synchronous_Asynchronous.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity D_FLIPFLOP is 
+  port (
+    
+  ):
+
+  end D_FLIPFLOP;
+
+  architecture Behavioral_FlipFlop_Mode of D_FLIPFLOP is
+    process (clk) -- synchronous
+    begin
+      if (clk' event and clk = '1') then 
+        if (rest = '1') then
+          Q = '0';
+        else
+          Q =  D;
+        end if;
+      end if;
+
+
+--   process (clk,rest) -- (clk, rest) // for asynchronous
+--     begin
+--       if (rest = '1') then
+--         Q <= '0';
+--       elsif(clk' event and clk = '1') then
+--         Q <= D;
+--       end if;
+
+    end process;
+
+end architecture Behavioral_FlipFlop_Mode;
