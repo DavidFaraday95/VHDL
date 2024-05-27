b10ccb86aa03199a67fec2cc091e26e81e243448
DavidFaraday95 <46838417+DavidFaraday95@users.noreply.github.com>
2024-05-27 21:23:24 +0200

    Update 01_nand_gate_tb.vhd

--- Basics/01_nand_gate_tb.vhd
+++ Basics/01_nand_gate_tb.vhd
@@ -6,6 +6,7 @@ entity nand_tb is
 end nand_tb;
 
 architecture tb of nand_tb is 
+  -- Component Declaration for the Unit Under Test (UUT)
   component nand_gate port (
     A, B : in std_logic;
     Y : out std_logic;
@@ -25,17 +26,19 @@ begin
     Y => ctb;
   ):
 
+  -- Stimulus process to simulate input signals
   stimulus_process: process
 
     begin 
-
+      -- Apply inputs
       atb <= '0'; btb <= '0'; wait for 10 ns;
       atb <= '0'; btb <= '1'; wait for 10 ns;
       atb <= '1'; btb <= '0'; wait for 10 ns;
       atb <= '1'; btb <= '1'; wait for 10 ns;
 
+      -- Wait for a while and then end simulation
       wait for 20ns;
-      wait;
+      wait;  -- Wait forever; this will stop the simulaion
 
   end process stimulus_process;
 
