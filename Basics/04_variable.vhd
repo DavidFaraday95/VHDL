de77b27de3b305127978025a2c4d81e9bc6d99a1
DavidFaraday95 <46838417+DavidFaraday95@users.noreply.github.com>
2024-05-31 00:37:00 +0200

    Update 04_variable.vhd

--- Basics/04_variable.vhd
+++ Basics/04_variable.vhd
@@ -12,18 +12,16 @@ entity counterUp is
 architecture counterup_2 of counterUp is 
   variable o_temp_var : std_logic_vector (3 downto 0) := "0000";
 begin
-   if (clk 'event and clk = '1') then
+    if (clk 'event and clk = '1') then
       if (rest = '1') then
-       o_temp_var := "0000";
+        o_temp_var := "0000";
       elsif (o_temp_var = "1111") then
         o_temp_var := "0000";
       else
         o_temp_var := o_temp_var + 1;
       end if;
-
       o <= o_temp_var;
     end if;
-end process;
-
+      
 end counterup_2
         
